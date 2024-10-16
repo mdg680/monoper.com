c8cb22ff9f99f1d8e3ae2f842dc29f4aa61e168c
mdg680 <monoper@gmai.com>
2024-10-16 23:01:16 +0200

    Basic flask backend setup with some basic test posts

---
+++ backend/app.py
@@ -0,0 +1,28 @@
+import flask
+from flask import Flask, jsonify  # , render_template, request
+
+# Current source: https://medium.com/@arif555/containerizing-a-full-stack-web-application-a-step-by-step-guide-e9374f748177
+def get_posts() -> flask.Response:
+    """ Returns a list of posts
+        :returns: A list of posts
+    """
+    return jsonify(
+        [
+            {
+                'author': 'John Doe',
+                'title': 'First Post',
+                'content': 'This is the first post',
+                'date_posted': 'April 20, 2018'
+            },
+            {
+                'author': 'Jane Doe',
+                'title': 'Second Post',
+                'content': 'This is the second post',
+                'date_posted': 'April 21, 2018'
+            }
+        ]
+    )
+
+if __name__ == '__main__':
+    app = Flask(__name__)
+    app.run(debug=True, host='0.0.0.0', port=8000)
