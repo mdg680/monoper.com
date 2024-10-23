78d8e8aee87d3e720efda595a72bcdecea6ae3ab
mdg680 <monoper@gmai.com>
2024-10-24 01:39:31 +0200

    Flake8 fixes

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,32 @@
 import flask
-from flask import Flask, jsonify  # , render_template, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-# Current source: https://medium.com/@arif555/containerizing-a-full-stack-web-application-a-step-by-step-guide-e9374f748177
-@app.route('/api/posts', methods=['GET'])
+
+
+# Current source: https://medium.com/@arif555/containerizing-a-full-stack-web-application-a-step-by-step-guide-e9374f748177 # noqa
+@app.route("/api/posts", methods=["GET"])
 def get_posts() -> flask.Response:
-    """ Returns a list of posts
-        :returns: A list of posts
+    """Returns a list of posts
+    :returns: A list of posts
     """
     return jsonify(
         [
             {
-                'author': 'John Doe',
-                'title': 'First Post',
-                'content': 'This is the first post',
-                'date_posted': 'April 20, 2018'
+                "author": "John Doe",
+                "title": "First Post",
+                "content": "This is the first post",
+                "date_posted": "April 20, 2018",
             },
             {
-                'author': 'Jane Doe',
-                'title': 'Second Post',
-                'content': 'This is the second post',
-                'date_posted': 'April 21, 2018'
-            }
+                "author": "Jane Doe",
+                "title": "Second Post",
+                "content": "This is the second post",
+                "date_posted": "April 21, 2018",
+            },
         ]
     )
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)
